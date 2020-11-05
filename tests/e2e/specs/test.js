cc34d2ec301eb41ca8cd8b1d9b58d418ab232c3d
William C. Duraney <william.duraney@gmail.com>
2020-11-05 13:55:59 -0500

    start playing with e2e testing

--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -2,20 +2,17 @@
 // https://nightwatchjs.org/guide
 
 module.exports = {
-  "default e2e tests": browser => {
+  "step 1: open app": browser => {
     browser
       .init()
       .waitForElementVisible("#app")
-      .assert.elementPresent(".hello")
-      .assert.containsText("h1", "Welcome to Your Vue.js App")
+      .assert.elementPresent("#the-header")
+      .assert.containsText("h1", "Vue Todo App")
       .assert.elementCount("img", 1)
-      .end();
+      .assert.elementPresent("#app-container");
   },
 
   "example e2e test using a custom command": browser => {
-    browser
-      .openHomepage()
-      .assert.elementPresent(".hello")
-      .end();
+    browser.assert.elementPresent("#app").end();
   }
 };
